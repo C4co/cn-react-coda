4a7d79566788397c30a3828ad9cc9fddd289ec3d
Carlos Costa <carllos.nc@gmail.com>
2021-02-21 02:49:42 -0300

    Updagrading example page.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,232 @@
-import React from 'react'
+import React, { useState } from "react"
+import { Coda } from "cn-react-coda"
+import styled, { css, ThemeProvider } from "styled-components"
+import LOGO_LIGHT from "./assets/logo-light.svg"
+import LOGO_DARK from "./assets/logo-dark.svg"
+import { USAGE, HTML_XML, CSS, JAVASCRIPT, RUBY, PHP } from "./examples"
+import { BiCheck } from "react-icons/bi"
 
-import { ExampleComponent } from 'cn-react-coda'
-import 'cn-react-coda/dist/index.css'
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  ${props => css`
+    background: ${props.theme.BACKGROUND};
+  `}
+`
+
+const Content = styled.div`
+  max-width: 800px;
+  width: 100%;
+  padding: 30px 10px 50px 10px;
+  margin: 0 auto;
+`
+
+const Logo = styled.img`
+  width: 100%;
+  margin: 0 auto;
+  display: block;
+  max-width: 150px;
+`
+
+const Description = styled.span`
+  width: 100%;
+  display: block;
+  text-align: center;
+
+  ${props => css`
+    color: ${props.theme.TEXT};
+  `}
+`
+
+const Install = styled.div`
+  text-align: center;
+  padding: 10px;
+  font-size: 1.3em;
+
+  ${props => css`
+    color: ${props.theme.INFO};
+  `}
+`
+
+const BigTitle = styled.h1`
+  font-size: 1.7em;
+  font-weight: normal;
+  width: 100%;
+  text-align: center;
+
+  ${props => css`
+    color: ${props.theme.TITLE};
+  `}
+`
+
+const Feature = styled.div`
+  display: flex;
+  padding: 6px 10px 6px 10px;
+  margin-bottom: 5px;
+  align-items: center;
+
+  ${props => css`
+    color: ${props.theme.TEXT};
+    background-color: ${props.theme.FEATURE};
+  `}
+`
+
+const FeatureIcon = styled(BiCheck)`
+  font-size: 30px;
+  margin-right: 20px;
+
+  ${props => css`
+    color: ${props.theme.SUCCESS};
+  `}
+`
+
+const ChangeTheme = styled.div`
+  width: 175px;
+  height: 40px;
+  margin: 0 auto;
+  border-radius: 100px;
+  bottom: 10px;
+  right: 10px;
+  cursor: pointer;
+  display: flex;
+  font-size: 1em;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+
+  ${props => css`
+    background: ${props.theme.CHANGE};
+    color: ${props.theme.BACKGROUND};
+  `}
+`
+
+const ChangeThemeIcon = styled.span`
+  font-size: 1.4em;
+  margin-right: 10px;
+`
+
+const dark = {
+  THEME: "dark",
+  BACKGROUND: "#1c1c30",
+  TEXT: "#9292c6",
+  INFO: "#33acff",
+  FEATURE: "#262641",
+  SUCCESS: "#00cc66",
+  TITLE: "#ffffff",
+  CODA_THEME: "dark",
+  LOGO: LOGO_DARK,
+  CHANGE: "#f1f1f1",
+  CHANGE_ICON: "🌞",
+  CHANGE_TEXT: "Ligth theme"
 }
 
-export default App
+const light = {
+  THEME: "light",
+  BACKGROUND: "#f1f1f1",
+  TEXT: "#3a3a3a",
+  INFO: "#0033ff",
+  FEATURE: "#ffffff",
+  SUCCESS: "#009933",
+  TITLE: "#111111",
+  CODA_THEME: "light",
+  LOGO: LOGO_LIGHT,
+  CHANGE: "#1c1c30",
+  CHANGE_ICON: "🎃",
+  CHANGE_TEXT: "Dark theme"
+}
+
+export default function App(){
+  const [ theme, setTheme ] = useState(dark)
+
+  function onThemeChange(){
+    console.log("Here!")
+
+    if(theme.THEME === "light"){
+      setTheme(dark)
+    }
+
+    if(theme.THEME === "dark"){
+      setTheme(light)
+    }
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Content>
+          <Logo src={theme.LOGO}/>
+
+          <br/><br/>
+
+          <Description>
+            A simple react component to write code snipets
+          </Description>
+
+          <br/>
+
+          <Install>
+            npm install cn-react-coda
+          </Install>
+
+          <br/>
+          <br/>
+
+          <ChangeTheme onClick={onThemeChange} >
+            <ChangeThemeIcon>
+              { theme.CHANGE_ICON }
+            </ChangeThemeIcon>
+
+            { theme.CHANGE_TEXT }
+          </ChangeTheme>
+
+          <br/>
+          <br/>
+
+          <Feature>
+            <FeatureIcon/> Copy code to clipboard with one click
+          </Feature>
+
+          <Feature>
+            <FeatureIcon/> Support 191 languages (highlight.js)
+          </Feature>
+
+          <Feature>
+            <FeatureIcon/> Responsive
+          </Feature>
+
+          <Feature>
+            <FeatureIcon/> Light and dark theme
+          </Feature>
+
+          <br/><br/>
+
+          <Coda lang="html" theme={theme.CODA_THEME} title="usage.tsx" code={USAGE} />
+
+          <br/><br/>
+
+          <BigTitle> Support 191 languages </BigTitle>
+
+          <br/>
+
+          <Description>
+            Check some examples
+          </Description>
+
+          <br/><br/>
+
+          <Coda lang="html" theme={theme.CODA_THEME} title="example.html" code={HTML_XML} />
+          <br/>
+          <Coda lang="css" theme={theme.CODA_THEME} title="example.css" code={CSS} />
+          <br/>
+          <Coda lang="javascript" theme={theme.CODA_THEME} title="example.js" code={JAVASCRIPT} />
+          <br/>
+          <Coda lang="ruby" theme={theme.CODA_THEME} title="example.rb" code={RUBY} />
+          <br/>
+          <Coda lang="php" theme={theme.CODA_THEME} title="example.php" code={PHP} />
+        </Content>
+      </Container>
+    </ThemeProvider>
+  )
+}
---
+++ example/src/examples.ts
@@ -0,0 +1,149 @@
+export const USAGE = `
+import React from "react"
+import { Coda } from "cn-react-coda"
+
+const example = \`
+  function fn(){
+    console.log("Hello world!")
+  }
+\`
+
+export default function App(){
+  return (
+    <Coda
+      lang="js"
+      theme="dark"
+      title="example.js"
+      code={example}
+    />
+  )
+}
+
+`
+
+export const HTML_XML = `
+<!DOCTYPE html>
+<title>Title</title>
+
+<style>body {width: 500px;}</style>
+
+<script type="application/javascript">
+  function $init() {return true;}
+</script>
+
+<body>
+  <p checked class="title" id='title'>Title</p>
+  <!-- here goes the rest of the page -->
+</body>
+`
+
+export const CSS = `
+@font-face {
+  font-family: Chunkfive; src: url('Chunkfive.otf');
+}
+
+body, .usertext {
+  color: #F0F0F0; background: #600;
+  font-family: Chunkfive, sans;
+  --heading-1: 30px/32px Helvetica, sans-serif;
+}
+
+@import url(print.css);
+@media print {
+  a[href^=http]::after {
+    content: attr(href)
+  }
+}
+`
+
+export const JAVASCRIPT = `
+function $initHighlight(block, cls) {
+  try {
+    if (cls.search(\/\\bno\-highlight\\b\/) != -1)
+      return process(block, true, 0x0F) +\` class="\${cls}"\`;
+  } catch (e) {
+    /* handle exception */
+  }
+  for (var i = 0 / 2; i < classes.length; i++) {
+    if (checkCondition(classes[i]) === undefined)
+      console.log('undefined');
+  }
+
+  return (
+    <div>
+      <web-component>{block}</web-component>
+    </div>
+  )
+}
+
+export  $initHighlight;
+`
+export const RUBY = `
+# The Greeter class
+class Greeter
+  def initialize(name)
+    @name = name.capitalize
+  end
+
+  def salute
+    puts "Hello #{@name}!"
+  end
+end
+
+g = Greeter.new("world")
+g.salute
+`
+
+export const PHP = `
+require_once 'Zend/Uri/Http.php';
+
+namespace Location\\Web;
+
+interface Factory
+{
+    static function _factory();
+}
+
+abstract class URI extends BaseURI implements Factory
+{
+    abstract function test();
+
+    public static $st1 = 1;
+    const ME = "Yo";
+    var $list = NULL;
+    private $var;
+
+    /**
+     * Returns a URI
+     *
+     * @return URI
+     */
+    static public function _factory($stats = array(), $uri = 'http')
+    {
+        echo __METHOD__;
+        $uri = explode(':', $uri, 0b10);
+        $schemeSpecific = isset($uri[1]) ? $uri[1] : '';
+        $desc = 'Multiline description';
+
+        // Security check
+        if (!ctype_alnum($scheme)) {
+            throw new Zend_Uri_Exception('Illegal scheme');
+        }
+
+        $this->var = 0 - self::$st;
+        $this->list = list(Array("1"=> 2, 2=>self::ME, 3 => \\Location\\Web\\URI::class));
+
+        return [
+            'uri'   => $uri,
+            'value' => null,
+        ];
+    }
+}
+
+echo URI::ME . URI::$st1;
+
+__halt_compiler () ; datahere
+datahere
+datahere */
+datahere
+`
