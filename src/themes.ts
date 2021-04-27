eae437c3ef0c0204d4b45f417aab67ce257ab8bb
Carlos Costa <carllos.nc@gmail.com>
2021-04-27 20:04:27 -0300

    Removing borders.

--- src/themes.ts
+++ src/themes.ts
@@ -3,7 +3,6 @@ import {lighten, darken} from "polished"
 
 export const DARK = {
   BACKGROUND: "#22223A",
-  BORDERS: lighten(0.08, "#22223A"),
   NUMBERS: lighten(0.3, "#22223A"),
   TITLE: lighten(0.5, "#22223A"),
   SCROLL_TRACK: lighten(0.1, "#22223A"),
@@ -17,7 +16,6 @@ export const DARK = {
 
 export const LIGHT = {
   BACKGROUND: "#FFFFFF",
-  BORDERS: darken(0.1, "#FFFFFF"),
   NUMBERS: darken(0.5, "#FFFFFF"),
   TITLE: darken(0.7, "#FFFFFF"),
   SCROLL_TRACK: darken(0.1, "#FFFFFF"),
