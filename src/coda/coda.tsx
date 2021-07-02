897f53c88d5d29016ca9cf5ca279c48ac91c835d
Carlos Costa <carllos.nc@gmail.com>
2021-07-02 15:37:16 -0300

    Removing font-size feature.

--- src/coda/coda.tsx
+++ src/coda/coda.tsx
@@ -1,4 +1,3 @@
-import "jetbrains-mono"
 import React, {useRef, useState} from "react"
 import {ThemeProvider} from "styled-components"
 import {DARK, LIGHT} from "../themes"
@@ -7,9 +6,7 @@ import {
   CodaContainer,
   CodaContent,
   CodaCopy,
-  CodaFontSize,
   CodaControls,
-  CodaIcon,
   CodaInput,
   CodaTitle,
   CodaHeader
@@ -17,16 +14,15 @@ import {
 
 interface CodaProps {
   code: string
-  controls?: boolean
   title?: string | boolean
   lang?: string
   theme: string
+  showLineNumbers: boolean
 }
 
 export function Coda(props: CodaProps) {
   const input = useRef<HTMLTextAreaElement>(null)
   const [copied, setCopied] = useState(false)
-  const [fontSize, setFontSize] = useState(18)
 
   let theme = DARK
 
@@ -36,18 +32,6 @@ export function Coda(props: CodaProps) {
 
   if (props.theme === "dark") {
     theme = DARK
-  }
-
-  function IncrementFontSize() {
-    if (fontSize < 22) {
-      setFontSize(fontSize + 1)
-    }
-  }
-
-  function DecrementFontSize() {
-    if (fontSize > 14) {
-      setFontSize(fontSize - 1)
-    }
   }
 
   function copyToClipboard() {
@@ -62,69 +46,57 @@ export function Coda(props: CodaProps) {
 
   return (
     <ThemeProvider theme={theme}>
-      <CodaContainer fontSize={fontSize} data-testid="coda-container">
+      <CodaContainer data-testid="coda-container">
         <CodaContent data-testid="coda-content">
-          <CodaHeader>
-            {/* Title */}
 
-            {props.title && (
+          {props.title && (
+            <CodaHeader data-testid="coda-header">
               <CodaTitle data-testid="coda-title">
-                <CodaIcon data-testid="coda-icon" />
                 {props.title}
               </CodaTitle>
-            )}
-
-            {/* Bar */}
-
-            {props.controls && (
-              <CodaControls data-testid="coda-controls">
-                <CodaFontSize
-                  data-testid="coda-fontsize-decrement"
-                  onClick={DecrementFontSize}>
-                  A-
-                </CodaFontSize>
-
-                <CodaFontSize
-                  data-testid="coda-fontsize-increment"
-                  onClick={IncrementFontSize}>
-                  A+
-                </CodaFontSize>
-
-                <CodaInput
-                  data-testid="coda-input"
-                  readOnly
-                  ref={input}
-                  value={props.code.trim()}
-                />
-
-                <CodaCopy
-                  data-testid="coda-copy"
-                  copied={copied}
-                  onClick={copyToClipboard}>
-                  {copied ? "Copied!" : "Copy code"}
-                </CodaCopy>
-              </CodaControls>
-            )}
-          </CodaHeader>
+            </CodaHeader>
+          )}
 
           {/* Content */}
 
           <CodaCode
             data-testid="coda-code"
-            showLineNumbers
+            showLineNumbers={props.showLineNumbers}
             language={props.lang}
             style={theme.SCHEME}>
             {props.code.trim()}
           </CodaCode>
+
         </CodaContent>
+
+        {/* Controls */}
+
+        <CodaControls className="coda__controls" data-testid="coda-controls">
+          <CodaInput
+            data-testid="coda-input"
+            readOnly
+            ref={input}
+            value={props.code.trim()}
+          />
+
+          <CodaCopy
+            data-testid="coda-copy"
+            copied={copied}
+            onClick={copyToClipboard}>
+            {copied ? "Copied!" : "Copy code"}
+          </CodaCopy>
+        </CodaControls>
+
       </CodaContainer>
     </ThemeProvider>
   )
 }
 
 Coda.defaultProps = {
+  code: "",
   lang: "jsx",
   controls: false,
   title: false,
-  theme: "dark"
+  theme: "dark",
+  showLineNumbers: false
 }
