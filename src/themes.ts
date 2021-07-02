569afaaff58853e296f34d9d4f4c3582b0f9fa9a
Carlos Costa <carllos.nc@gmail.com>
2021-07-02 15:35:55 -0300

    Updating colors.

--- src/themes.ts
+++ src/themes.ts
@@ -4,6 +4,7 @@ import {lighten, darken} from "polished"
 export const DARK = {
   BACKGROUND: "#22223A",
   NUMBERS: lighten(0.3, "#22223A"),
+  BORDER: lighten(0.1, "#22223A"),
   TITLE: lighten(0.5, "#22223A"),
   SCROLL_TRACK: lighten(0.1, "#22223A"),
   SCROLL_THUMB: lighten(0.17, "#22223A"),
@@ -17,11 +18,12 @@ export const DARK = {
 export const LIGHT = {
   BACKGROUND: "#FFFFFF",
   NUMBERS: darken(0.5, "#FFFFFF"),
+  BORDER: darken(0.1, "#FFFFFF"),
   TITLE: darken(0.7, "#FFFFFF"),
   SCROLL_TRACK: darken(0.1, "#FFFFFF"),
   SCROLL_THUMB: darken(0.2, "#FFFFFF"),
-  BUTTON: "#0099cc",
-  BUTTON_TEXT: "#FFFFFF",
+  BUTTON: "#FFFFFF",
+  BUTTON_TEXT: "#333333",
   BUTTON_SUCCESS: "#00b057",
   BUTTON_SUCCESS_TEXT: "#ffffff",
   SCHEME: vs
