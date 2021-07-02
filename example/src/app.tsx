27605e1c43dd6c24b372a73ee33d2b9656906c7f
Carlos Costa <carllos.nc@gmail.com>
2021-07-02 15:38:37 -0300

    Some refacts.

--- example/src/app.tsx
+++ example/src/app.tsx
@@ -3,13 +3,14 @@ import { Coda } from "cn-react-coda"
 import styled, { css, ThemeProvider } from "styled-components"
 import LOGO_LIGHT from "./assets/logo-light.svg"
 import LOGO_DARK from "./assets/logo-dark.svg"
-import { USAGE, HTML_XML, CSS, JAVASCRIPT, RUBY, PHP, C_PLUS_PLUS } from "./examples"
+import { USAGE, IMPORT_CODA, CSS, C_PLUS_PLUS, HTML_XML, JAVASCRIPT, PHP, RUBY } from "./examples"
 import { BiCheck } from "react-icons/bi"
 
 const Container = styled.div`
   width: 100%;
   min-height: 100vh;
   position: relative;
+  text-align: center;
 
   ${props => css`
     background: ${props.theme.BACKGROUND};
@@ -27,7 +28,7 @@ const Title = styled.h1`
   font-size: 3.4em;
   text-align: center;
   margin-bottom: 30px;
-  font-weight: 300;
+  font-weight: bold;
 
   ${props => css`
     color: ${props.theme.TEXT};
@@ -37,20 +38,9 @@ const Title = styled.h1`
 const Description = styled.span`
   width: 100%;
   display: block;
-  text-align: center;
 
   ${props => css`
     color: ${props.theme.TEXT};
-  `}
-`
-
-const Install = styled.div`
-  text-align: center;
-  padding: 10px;
-  font-size: 1.3em;
-
-  ${props => css`
-    color: ${props.theme.INFO};
   `}
 `
 
@@ -58,7 +48,6 @@ const BigTitle = styled.h1`
   font-size: 1.7em;
   font-weight: normal;
   width: 100%;
-  text-align: center;
 
   ${props => css`
     color: ${props.theme.TITLE};
@@ -70,6 +59,8 @@ const Feature = styled.div`
   padding: 12px;
   margin-bottom: 5px;
   align-items: center;
+  text-align: left;
+  border-radius: 3px;
 
   ${props => css`
     color: ${props.theme.TEXT};
@@ -87,14 +78,14 @@ const FeatureIcon = styled(BiCheck)`
 `
 
 const ChangeTheme = styled.div`
-  width: 175px;
-  height: 40px;
   margin: 0 auto;
   border-radius: 100px;
   bottom: 10px;
   right: 10px;
   cursor: pointer;
-  display: flex;
+  display: inline-flex;
+  margin: 0 auto;
+  padding: 10px 20px 10px 20px;
   font-size: 1em;
   justify-content: center;
   align-items: center;
@@ -104,11 +95,6 @@ const ChangeTheme = styled.div`
     background: ${props.theme.CHANGE};
     color: ${props.theme.BACKGROUND};
   `}
-`
-
-const ChangeThemeIcon = styled.span`
-  font-size: 1.4em;
-  margin-right: 10px;
 `
 
 const dark = {
@@ -122,7 +108,6 @@ const dark = {
   CODA_THEME: "dark",
   LOGO: LOGO_DARK,
   CHANGE: "#f1f1f1",
-  CHANGE_ICON: "🌞",
   CHANGE_TEXT: "Ligth theme"
 }
 
@@ -137,7 +122,6 @@ const light = {
   CODA_THEME: "light",
   LOGO: LOGO_LIGHT,
   CHANGE: "#1c1c30",
-  CHANGE_ICON: "🎃",
   CHANGE_TEXT: "Dark theme"
 }
 
@@ -165,23 +149,16 @@ export default function App(){
             A simple react component to write code snipets
           </Description>
 
-          <Install>
-            npm install cn-react-coda
-          </Install>
-
-          <br/>
-          <br/>
+          <br />
+          <br />
 
           <ChangeTheme onClick={onThemeChange} >
-            <ChangeThemeIcon>
-              { theme.CHANGE_ICON }
-            </ChangeThemeIcon>
-
             { theme.CHANGE_TEXT }
           </ChangeTheme>
 
-          <br/>
-          <br/>
+          <br />
+          <br />
+          <br />
 
           <Feature>
             <FeatureIcon/> Copy code to clipboard with one click
@@ -199,37 +176,37 @@ export default function App(){
             <FeatureIcon/> Light and dark theme
           </Feature>
 
-          <br/><br/>
+          <br />
+          <br />
 
           <BigTitle> How to use? </BigTitle>
 
-          <br/>
+          <br />
 
-          <Coda lang="html" theme={theme.CODA_THEME} title="usage.tsx" code={USAGE} />
-
-          <br/><br/>
-
-          <BigTitle> Support 191 languages </BigTitle>
+          <Coda lang="html" theme={theme.CODA_THEME} code={IMPORT_CODA} />
 
           <br/>
 
-          <Description>
-            Check some examples
-          </Description>
+          <Coda lang="js" theme={theme.CODA_THEME} code={USAGE} controls />
 
-          <br/>
+          <br />
 
-          <Coda controls={true} lang="html" theme={theme.CODA_THEME} title="example.html" code={HTML_XML} />
+          <BigTitle> Check some examples </BigTitle>
+
+          <br />
+
+          <Coda controls={true} lang="html" showLineNumbers theme={theme.CODA_THEME} title="example.html" code={HTML_XML} />
           <br/>
-          <Coda controls={true} lang="css" theme={theme.CODA_THEME} title="example.css" code={CSS} />
+          <Coda controls={true} lang="css" showLineNumbers theme={theme.CODA_THEME} title="example.css" code={CSS} />
           <br/>
-          <Coda controls={true} lang="javascript" theme={theme.CODA_THEME} title="example.js" code={JAVASCRIPT} />
+          <Coda controls={true} lang="javascript" showLineNumbers theme={theme.CODA_THEME} title="example.js" code={JAVASCRIPT} />
           <br/>
-          <Coda controls={true} lang="ruby" theme={theme.CODA_THEME} title="example.rb" code={RUBY} />
+          <Coda controls={true} lang="ruby" showLineNumbers theme={theme.CODA_THEME} title="example.rb" code={RUBY} />
           <br/>
-          <Coda controls={true} lang="php" theme={theme.CODA_THEME} title="example.php" code={PHP} />
+          <Coda controls={true} lang="php" showLineNumbers theme={theme.CODA_THEME} title="example.php" code={PHP} />
           <br/>
-          <Coda controls={true} lang="c++" theme={theme.CODA_THEME} title="example.cpp" code={C_PLUS_PLUS} />
+          <Coda controls={true} lang="c++" showLineNumbers theme={theme.CODA_THEME} title="example.cpp" code={C_PLUS_PLUS} />
+
         </Content>
       </Container>
     </ThemeProvider>
